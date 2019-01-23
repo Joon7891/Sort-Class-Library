fedc2ebd259b62bcadd9fc0fb4dbdd4534a6673e
Joon Song <joon7891@gmail.com>
2019-01-23 10:11:11 -0500

    Initial commit

---
+++ SortHelper/Sort.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortHelper
+{
+    public class Sort<T>
+    {
+        public delegate bool Comparator(T a, T b);
+
+        public T[] BubbleSort()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T[] InsertionSort()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T[] MergeSort()
+        {
+            throw new NotImplementedException();
+        }
+
+        private T[] Merge()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
